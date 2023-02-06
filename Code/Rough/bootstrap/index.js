28f774b06519bfcb9f73d88e7caf0d6c146e09df
almunna <124546006+almunna@users.noreply.github.com>
2023-02-06 19:45:13 +0600

    Add files via upload

---
+++ Code/Rough/bootstrap/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var divs = $('.mydivs>div');
+    var now = 0; // currently shown div
+    divs.hide().first().show();
+    $("button[name=next]").click(function (e) {
+        divs.eq(now).hide();
+        now = (now + 1 < divs.length) ? now + 1 : 0;
+        divs.eq(now).show(); // show next
+    });
+    $("button[name=prev]").click(function (e) {
+        divs.eq(now).hide();
+        now = (now > 0) ? now - 1 : divs.length - 1;
+        divs.eq(now).show(); // or .css('display','block');
+        //console.log(divs.length, now);
+    });
+});
